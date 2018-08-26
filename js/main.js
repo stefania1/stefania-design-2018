79697cf4dce7fcf45da3076349cd9272284c9b53
stefania1 <arielmonster@gmail.com>
2018-08-26 20:11:07 +0200

    New changes on work part

--- js/main.js
+++ js/main.js
@@ -13,17 +13,11 @@
 
 // Smooth scoll page effect
 $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
+    $('a').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top
+        }, 500);
+        return false;
     });
 });
 
