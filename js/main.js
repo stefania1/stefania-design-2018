3899a21d95924784c361167c3851e16257fcd836
stefania1 <arielmonster@gmail.com>
2018-08-27 14:06:40 +0200

    Last changes

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@
 
 })(jQuery);
 
+
 // Smooth scoll page effect
 $(function() {
     $('a').click(function(){
@@ -19,5 +20,21 @@ $(function() {
         }, 500);
         return false;
     });
+});
+
+
+// Portfolio 
+$(function() {
+    var selectedClass = "";
+    $(".fil-cat").click(function(){ 
+    selectedClass = $(this).attr("data-rel"); 
+ $("#portfolio").fadeTo(100, 0.1);
+    $("#portfolio a").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+setTimeout(function() {
+  $("."+selectedClass).fadeIn().addClass('scale-anm');
+  $("#portfolio").fadeTo(300, 1);
+}, 300); 
+    
+});
 });
 
